21e91efdc75849b00e2073c218f933bef4531731
verna <vernajs@gmail.com>
2018-06-12 11:52:10 -0700

    Add Edit and Delete function

---
+++ albumV.3/src/main.ts
@@ -0,0 +1,13 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import $ from 'jquery';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import 'bootstrap';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
