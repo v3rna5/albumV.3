668e26b43c02ec6ef5915698cd72e2dda704b12d
verna <vernajs@gmail.com>
2018-06-12 09:36:49 -0700

    Album V.2 completed

---
+++ albumV.2/src/app/album.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+import { ALBUMS } from './mock-albums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Injectable()
+export class AlbumService {
+  albums: FirebaseListObservable<any[]>;
+
+  constructor(private database: AngularFireDatabase) {
+     this.albums = database.list('albums');
+   }
+
+
+  getAlbums() {
+    return this.albums;
+  }
+  addAlbum(newAlbum: Album) {
+  this.albums.push(newAlbum);
+}
+
+getAlbumById(albumId: string){
+return this.database.object('albums/' + albumId);
+      // for (var i = 0; i <= ALBUMS.length - 1; i++) {
+      //   if (ALBUMS[i].id === albumId) {
+      //     return ALBUMS[i];
+      //   }
+      // }
+    }
+
+  }
---
+++ albumV.2/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AdminComponent } from './admin/admin.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    AboutComponent,
+    MarketplaceComponent,
+    AlbumDetailComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
